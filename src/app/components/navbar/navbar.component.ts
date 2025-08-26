0781eec5edb1950e411d3de63bb31010e28b1344
Alonso <jalonsojic@gmail.com>
2025-08-26 02:16:32 -0600

    v2.4 Agregados mas responsive, prueba para toggle

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { faGithub, faLinkedin, faInstagram, faDiscord } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss'],
-    standalone: false
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  standalone: false
 })
 export class NavbarComponent {
   faGithub = faGithub;
   faLinkedin = faLinkedin;
   faInstagram = faInstagram;
   faDiscord = faDiscord;
+
+  isMenuCollapsed = true;
+
+  toggleMenu() {
+    this.isMenuCollapsed = !this.isMenuCollapsed;
+  }
 }
